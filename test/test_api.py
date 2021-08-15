86f2c055a71e199d9b350d2ef17c4cba0e142020
Daniel Alves Dias <juninhoboni@gmail.com>
2021-08-15 09:03:06 -0300

    Ajuste na autenticação

--- test/test_api.py
+++ test/test_api.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-import json
 from api import app
 
 client = TestClient(app)
@@ -10,19 +9,10 @@ def test_index():
     assert response.json() == {"msg": "Este projeto não contém front-end"}
 
 def test_request_token():
-    mimetype = 'application/json'
-    headers = {
-        'Content-Type': mimetype,
-        'Accept': mimetype
-    }
-    data = {
-            'username':'teste', 
-            'password':'secret'
-    }
-
-    response = client.post("/token/", data=json.dumps(data), headers=headers)
+    response = client.post("/token/?username=teste&password=secret")
     assert response.status_code == 200
-    #assert response.json() == {"msg": "Este projeto não contém front-end"}
+    assert response.json()['token_type'] ==  "bearer"
+    
 '''def test_read_item():
     response = client.get("/", headers={"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0ZSIsImV4cCI6MTYyODcyMzIxOH0.AWo88LtqU8QHrdvelQ1-ucYyC5zaztLGX8kVJoi7zBs"})
     assert response.status_code == 200
