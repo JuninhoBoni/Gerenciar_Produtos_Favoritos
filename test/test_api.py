e77ee9340c7e2cacf26f34df4f8075c556e7bc23
Daniel Alves Dias <juninhoboni@gmail.com>
2021-08-15 08:34:56 -0300

    Validação do token

--- test/test_api.py
+++ test/test_api.py
@@ -9,6 +9,10 @@ def test_index():
     assert response.status_code == 200
     assert response.json() == {"msg": "Este projeto não contém front-end"}
 
+def test_request_token():
+    response = client.post("/token2",json={'username':'teste', 'password':'secret'})
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Este projeto não contém front-end"}
 '''def test_read_item():
     response = client.get("/", headers={"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0ZSIsImV4cCI6MTYyODcyMzIxOH0.AWo88LtqU8QHrdvelQ1-ucYyC5zaztLGX8kVJoi7zBs"})
     assert response.status_code == 200
